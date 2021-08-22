898a601b167b52df9b98a4119177be4c3008fa88
PataKHFung <patakhfung@gmail.com>
2021-08-22 23:15:25 +0100

    added search and search result page

--- components/Header.js
+++ components/Header.js
@@ -1,53 +1,105 @@
-import React, { Component } from 'react';
+import React, {Component, useState} from 'react';
 import Image from 'next/image';
 import {
   GlobeAltIcon,
   MenuIcon,
   UserCircleIcon,
   UserIcon,
+  UsersIcon,
   SearchIcon
 } from '@heroicons/react/solid';
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+import {DateRangePicker} from 'react-date-range';
+import {useRouter} from "next/router";
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
-        {/* left */}
-        <div className="relative flex items-center h-10 cursor-pointer my-auto">
-          <Image
-            src="https://links.papareact.com/qd3"
-            layout="fill"
-            objectFit="contain"
-            objectPosition="left"
-          />
+function Header({placeholder}) {
+  const router = useRouter();
+  const [searchInput, setSearchInput] = useState('');
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(new Date());
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'Selection'
+  }
+  const handleSelect = (ranges) => {
+    setStartDate(ranges.Selection.startDate);
+    setEndDate(ranges.Selection.endDate);
+  };
+  const resetInput = () => {
+    setSearchInput('');
+  }
+
+  const search = () => {
+    router.push({
+      pathname: '/search',
+      query: {
+        location: searchInput,
+        startDate: startDate.toISOString(),
+        endDate: endDate.toISOString(),
+        numOfGuests: numOfGuests
+      }
+    });
+  }
+  const [numOfGuests, setNumOfGuests] = useState(1);
+  return (
+    <header className="sticky top-0 z-50 grid grid-cols-3 bg-white shadow-md p-5 md:px-10">
+      {/* left */}
+      <div onClick={()=>router.push('/')} className="relative flex items-center h-10 cursor-pointer my-auto">
+        <Image
+          src="https://links.papareact.com/qd3"
+          layout="fill"
+          objectFit="contain"
+          objectPosition="left"
+        />
+      </div>
+
+      {/* Middle */}
+      <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-lg">
+        <input
+          value={searchInput} onChange={(e) => setSearchInput(e.target.value)}
+          className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
+          type="text"
+          placeholder={placeholder || "Start your search"}
+        />
+        <SearchIcon
+          className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2"/>
+      </div>
+
+      {/* Right */}
+      <div className="flex items-center space-x-4 justify-end text-gray-500">
+        <p className="hidden md:inline cursor-pointer">Become a host</p>
+        <GlobeAltIcon className="h-6 cursor-pointer"/>
+        <div className="flex items-center space-x-2 border-2 rounded-full">
+          <MenuIcon className="h-6"/>
+          <UserIcon className="h-6"/>
         </div>
-
-        {/* Middle */}
-        <div
-          className="flex items-center
-                md:border-2 rounded-full py-2
-                md:shadow-lg"
-        >
-          <input
-            className="flex-grow pl-5 bg-transparent outline-none text-sm text-gray-600 placeholder-gray-400"
-            type="text"
-            placeholder="Start your search"
+      </div>
+      {searchInput && (
+        <div className='flex flex-col col-span-3 mx-auto'>
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={["#FD5B61"]}
+            onChange={handleSelect}
           />
-          <SearchIcon className="hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
-        </div>
-
-        {/* Right */}
-        <div className="flex items-center space-x-4 justify-end text-gray-500">
-          <p className="hidden md:inline cursor-pointer">Become a host</p>
-          <GlobeAltIcon className="h-6 cursor-pointer" />
-          <div className="flex items-center space-x-2 border-2 rounded-full">
-            <MenuIcon className="h-6" />
-            <UserIcon className="h-6" />
+          <div className='flex items-center border-b mb-4'>
+            <h2 className='text-2xl flex-grow font-semibold'>Number of Guests</h2>
+            <UsersIcon className='h-5'/>
+            <input value={numOfGuests} onChange={(e) => setNumOfGuests(e.target.value)}
+                   min={1}
+                   type='number'
+                   className='w-12 pl-2 text-lg outline-none text-red-400'/>
+          </div>
+          <div className='flex'>
+            <button onClick={resetInput} className='flex-grow text-gray-500'>Cancel</button>
+            <button onClick={search} className='flex-grow text-red-400'>Search</button>
           </div>
         </div>
-      </header>
-    );
-  }
+      )}
+    </header>
+  );
 }
 
 export default Header;
